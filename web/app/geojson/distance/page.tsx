aff8ae63536119a308c3aef8e3ceb7bb12ace49a
Universe <universe@grida.co>
2024-02-07 16:38:34 +0900

    .

---
+++ web/app/geojson/distance/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const meta: Metadata = {
+  title: "GeoJson Distance Calculator",
+  description: "Calculate the distance with GeoJson data",
+};
+
+export default function GeoJsonDistanceCalculatorPage() {
+  return <main></main>;
+}
